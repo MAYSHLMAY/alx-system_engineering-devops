c7c6fb3fa3243d7a4d22f99e785d7f6405a787fc
Mikiyas <educationforlife424@gmail.com>
2024-08-15 20:45:51 +0300

    done

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,10 +7,11 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "ubuntu:0x16.api.advanced:v1.0.0 (by /u/micemonster)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
     results = response.json().get("data")
     return results.get("subscribers")
+
